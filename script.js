01fef5eb385a6e99ab41041e051036b10e73b3a0
rosebud4567 <miccolimichela2001@gmail.com>
2022-12-14 12:51:42 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelector("#green").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+
+document.querySelector("#red").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#yellow").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#blue").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#pink").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
